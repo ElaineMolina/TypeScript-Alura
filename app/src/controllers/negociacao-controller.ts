ae3ca95a52761a8f90cb1e57985533b4c5312893
ElaineMolina <elainemolina10@gmail.com>
2021-11-24 16:00:13 -0300

    Decorator de método

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -13,49 +13,49 @@ export class NegociacaoController {
   private negociacoesView = new NegociacoesViews('#negociacoesView', true);
   private mensagemView = new MensagemView('#mensagemView');
 
-    
-  constructor(){
-    this.inputData = <HTMLInputElement>document.querySelector('#data') ;
+
+  constructor() {
+    this.inputData = <HTMLInputElement>document.querySelector('#data');
     this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
     this.inputValor = document.querySelector('#valor') as HTMLInputElement;
     this.negociacoesView.update(this.negociacoes);
   }
   @logarTempoDeExecucao()
-  public adiciona(): void{
-    
+  public adiciona(): void {
+
     /*
       Fulano, você pode esconder isso?
       */
-     
+
     const negociacao = Negociacao.criaDe(
       this.inputData.value,
       this.inputQuantidade.value,
       this.inputValor.value
     );
-    if (!this.ehDiaUtil(negociacao.data)){
+    if (!this.ehDiaUtil(negociacao.data)) {
       this.mensagemView
-          .update('Apenas negociações em dias úteis são aceitas');
-          return;
+        .update('Apenas negociações em dias úteis são aceitas');
+      return;
     }
     this.negociacoes.adiciona(negociacao);
     this.limparFormulario();
     this.atualizaView();
-   
+
   }
 
-  private ehDiaUtil(data: Date){
-    return data.getDay() > DiasDaSemana.DOMINGO 
-    &&     data.getDay() < DiasDaSemana.SABADO;
+  private ehDiaUtil(data: Date) {
+    return data.getDay() > DiasDaSemana.DOMINGO
+      && data.getDay() < DiasDaSemana.SABADO;
   }
-  
-  private limparFormulario(): void{
+
+  private limparFormulario(): void {
     this.inputData.value = '';
     this.inputQuantidade.value = '';
     this.inputValor.value = '';
     this.inputData.focus();
   }
 
-  private atualizaView(): void{
+  private atualizaView(): void {
     this.negociacoesView.update(this.negociacoes);
     this.mensagemView.update('Negociação adicionada com sucesso');
   }
